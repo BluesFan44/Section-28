ccdf4b95572eb6698f9f094e5313b5799c31d780
BluesFan44 <bluesfan44@gmail.com>
2023-06-09 15:41:51 -0500

    useState

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,104 @@
+import React, { useCallback, useEffect, useState } from "react";
+import ErrorModal from "../UI/ErrorModal";
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
+
+function Ingredients() {
+  const [userIngredients, setUserIngredients] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    fetch(
+      "https://react-hooks-update-3f97a-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log("RENDERING INGREDIENTS", userIngredients);
+  }, [userIngredients]);
+
+  const filteredIngredientsHandler = useCallback(
+    (filteredIngredients) => setUserIngredients(filteredIngredients),
+    []
+  );
+
+  const addIngredientHandler = (ingredient) => {
+    setIsLoading(true);
+    fetch(
+      "https://react-hooks-update-3f97a-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        setIsLoading(false);
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
+  };
+  const removeIngredientHandler = (id) => {
+    setIsLoading(true);
+    fetch(
+      `https://react-hooks-update-3f97a-default-rtdb.firebaseio.com/ingredients/${id}.json`,
+      {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        setIsLoading(false);
+        setUserIngredients((prevIngredients) =>
+          prevIngredients.filter((ingredient) => ingredient.id !== id)
+        );
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        setError("Failed to delete the ingredient");
+      });
+  };
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  return (
+    <div className="App">
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
+
+      <section>
+        <Search onLoadIngredients={filteredIngredientsHandler} />
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
